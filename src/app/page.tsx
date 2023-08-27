7eca39b6bf7fbc3d8d02a194938c4055eb9a9380
anoopraju31 <anoopraju31@gmail.com>
2023-08-27 21:38:33 +0530

    Button Component: styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@/components/ui'
 import React from 'react'
 
 const HomePage = () => {
-	return <div className='text-green-500'>page</div>
+	return (
+		<div className='text-green-500'>
+			<Button> Hello World </Button>
+		</div>
+	)
 }
 
 export default HomePage
